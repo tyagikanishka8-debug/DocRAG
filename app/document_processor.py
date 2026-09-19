c63413ea6496bc76d160d8d1440dbb5695294c9e
Kanishka Tyagi <tyagikanishka8@gmail.com>
2026-09-20 00:27:18 +0530

    Add OCR support and automation PDF extraction

--- app/document_processor.py
+++ app/document_processor.py
@@ -1,5 +1,8 @@
 import pymupdf
+import pytesseract
+from PIL import Image
 from docx import Document
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 def extract_text_from_pdf(file_path):
@@ -13,6 +16,8 @@ def extract_text_from_pdf(file_path):
     document.close()
 
     return text
+
+
 def extract_text_from_docx(file_path):
     document = Document(file_path)
 
@@ -23,6 +28,38 @@ def extract_text_from_docx(file_path):
 
     return text
 
+
+def extract_text_with_ocr(file_path):
+    document = pymupdf.open(file_path)
+
+    text = ""
+
+    for page in document:
+        pixmap = page.get_pixmap()
+
+        image = Image.frombytes(
+            "RGB",
+            [pixmap.width, pixmap.height],
+            pixmap.samples
+        )
+
+        text += pytesseract.image_to_string(image)
+        text += "\n"
+
+    document.close()
+
+    return text
+
+def extract_text_from_pdf_auto(file_path):
+    text = extract_text_from_pdf(file_path)
+
+    if len(text.strip()) > 50:
+        return text
+
+    print("Very little text found. Using OCR...")
+    return extract_text_with_ocr(file_path)
+
+
 if __name__ == "__main__":
     pdf_text = extract_text_from_pdf("uploads/sample.pdf")
     print("PDF TEXT:")
@@ -32,3 +69,7 @@ if __name__ == "__main__":
     print("\nDOCX TEXT:")
     print(docx_text)
 
+    auto_text = extract_text_from_pdf_auto("uploads/sample.pdf")
+    print("\nAUTO PDF TEXT:")
+    print(auto_text)
+    
