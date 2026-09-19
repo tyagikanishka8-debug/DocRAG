76ef9ec37fe9f68ac82029709981c9a88b24af86
Kanishka Tyagi <tyagikanishka8@gmail.com>
2026-09-19 23:48:40 +0530

    Add DOCX text extraction

--- app/document_processor.py
+++ app/document_processor.py
@@ -1,4 +1,5 @@
 import pymupdf
+from docx import Document
 
 
 def extract_text_from_pdf(file_path):
@@ -12,6 +13,22 @@ def extract_text_from_pdf(file_path):
     document.close()
 
     return text
+def extract_text_from_docx(file_path):
+    document = Document(file_path)
+
+    text = ""
+
+    for paragraph in document.paragraphs:
+        text += paragraph.text + "\n"
+
+    return text
+
 if __name__ == "__main__":
-    text = extract_text_from_pdf("uploads/sample.pdf")
-    print(text)
+    pdf_text = extract_text_from_pdf("uploads/sample.pdf")
+    print("PDF TEXT:")
+    print(pdf_text)
+
+    docx_text = extract_text_from_docx("uploads/sample.docx")
+    print("\nDOCX TEXT:")
+    print(docx_text)
+
